56393589c3614dc337cb5bc9875a1e542c180912
Manasa Tipparam <7389027+manasa884@users.noreply.github.com>
2024-06-15 03:50:14 -0700

    undo default dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         )}
       >
         <StoreProvider>
-          <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <Providers themeProps={{ attribute: "class" }}>
             <div className="relative flex flex-col h-screen">
               <Navbar />
               <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
